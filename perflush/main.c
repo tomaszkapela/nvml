55486a0a2bc0f193bb44b21e72e727399787dee9
Piotr Balcer <piotr.balcer@intel.com>
2017-06-20 15:14:30 +0200

    perflush: fixes

--- perflush/main.c
+++ perflush/main.c
@@ -59,14 +59,14 @@ asm volatile(".byte 0x66; xsaveopt %0" : "+m" (*(volatile char *)(addr)));
  * Author: Pierre L'Ecuyer,
  * Source: http://www.iro.umontreal.ca/~lecuyer/myftp/papers/tausme2.ps
 */
-unsigned int
+static unsigned int
 randf(void)
 {
 	static unsigned int z1 = SEED, z2 = SEED, z3 = SEED, z4 = SEED;
 	unsigned int b;
 	b  = ((z1 << 6) ^ z1) >> 13;
 	z1 = ((z1 & 4294967294U) << 18) ^ b;
-	b  = ((z2 << 2) ^ z2) >> 27; 
+	b  = ((z2 << 2) ^ z2) >> 27;
 	z2 = ((z2 & 4294967288U) << 2) ^ b;
 	b  = ((z3 << 13) ^ z3) >> 21;
 	z3 = ((z3 & 4294967280U) << 7) ^ b;
@@ -262,6 +262,8 @@ read_flushed(void *addr, size_t data_size)
 		ntmemcpy(buf, caddr, data_size);
 	}
 
+	free(buf);
+
 	return 0;
 }
 
@@ -338,7 +340,8 @@ main(int argc, char *argv[])
 	random_data = malloc(FSIZE);
 	assert(random_data != NULL);
 	for (size_t i = 0; i < FSIZE; ++i) {
-		*random_data = (char)i;
+		/* not really random, but doesn't matter */
+		*(random_data + i) = (char)i;
 	}
 
 	struct timespec start, stop;
